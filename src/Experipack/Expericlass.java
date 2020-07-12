16704e9e88bc05e2da2d019877be2bd33660e2f2
anandpasapula <avs.pasapula@googlemail.com>
2020-07-12 04:18:01 +0100

    first commit

---
+++ src/Experipack/Expericlass.java
@@ -0,0 +1,13 @@
+package Experipack;
+
+public class Expericlass {
+
+    public void printmetod(){
+        System.out.println(("printfrommethod"));
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("First Print");
+    }
+}
